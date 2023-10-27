b7c0aa1efa3a082f744a833ca153750f612ecdd4
dgketchum <dgketchum@gmail.com>
2023-10-27 14:41:55 -0600

    associate a gridmet point with each field, get gridmet, and corrected gridmet for either eto or etr

--- gridmet_corrected/gridmet.py
+++ gridmet_corrected/gridmet.py
@@ -1,13 +1,18 @@
 import os
+from tqdm import tqdm
 from datetime import datetime
 
 import ee
-import rasterio
-from shapely.geometry.point import Point
 import geopandas as gpd
+from rasterstats import zonal_stats
 
 from landsat.ee_utils import is_authorized
 from gridmet_corrected.thredds import GridMet
+
+CRS_TRANSFORM = [0.041666666666666664,
+                 0, -124.78749996666667,
+                 0, -0.041666666666666664,
+                 49.42083333333334]
 
 
 def export_openet_correction_surfaces():
@@ -25,55 +30,88 @@ def export_openet_correction_surfaces():
                 i,
                 description=desc,
                 bucket='wudr',
+                dimensions='1386x585',
                 fileNamePrefix=desc,
+                crsTransform=CRS_TRANSFORM,
                 crs='EPSG:4326')
             task.start()
             print(desc)
 
 
-def gridmet_centroids(tif, fields, gridmet_points):
+def corrected_gridmet(fields, gridmet_points, fields_join, gridmet_csv_dir, gridmet_ras, variable='eto'):
+    """This depends on running 'Raster Pixels to Points' on a WGS Gridmet raster,
+     attributing GFID, lat, and lon in the attribute table, and saving to project crs: 5071.
+     GFID is an arbitrary identifier e.g., $row_number. It further depends on projecting the
+     rasters to EPSG:5071, usng the project.sh bash script"""
 
-    shapefile = gpd.read_file(fields)
-    bounds = shapefile.total_bounds
+    print('Find field-gridmet joins')
 
-    with rasterio.open(tif, masked=True, bounds=bounds) as src:
-        transform = src.transform
-        cell_size_x = transform.a
-        cell_size_y = -transform.e
-        rows, cols = src.shape
-        centroids = []
+    if variable == 'eto':
+        thredds_var = 'pet'
+    else:
+        thredds_var = variable
 
-        for row in range(rows):
-            for col in range(cols):
-                x = transform.c + col * cell_size_x + cell_size_x / 2
-                y = transform.f + row * cell_size_y + cell_size_y / 2
-                centroids.append((x, y))
+    gridmet_pts = gpd.read_file(gridmet_points)
+    fields = gpd.read_file(fields)
 
-    idx = [i for i in range(1, len(centroids) + 1)]
-    df = gpd.GeoDataFrame(index=idx, columns=['GFID', 'geometry'])
-    df['geometry'] = [Point(c) for c in centroids]
-    df.to_file(gridmet_points, crs='EPSG:4326')
+    rasters = [os.path.join(gridmet_ras, 'gridmet_corrected_{}_{}.tif'.format(variable, m)) for m in range(1, 13)]
 
+    gridmet_targets = {}
+    for i, field in tqdm(fields.iterrows(), total=fields.shape[0]):
+        min_distance = 1e13
+        closest_fid = None
 
-def corrected_gridmet(points_shp, out_dir_, start='1987-01-01', end='2021-12-31', variable='eto'):
-    df = gpd.read_file(points_shp)
+        for j, g_point in gridmet_pts.iterrows():
+            distance = field['geometry'].centroid.distance(g_point['geometry'])
 
-    for i, r in df.iterrows():
-        point = r['geometry']
-        lat, lon = point.y, point.x
-        g = GridMet('eto', start='1987-01-01', end='2021-12-31', lat=lat, lon=lon)
+            if distance < min_distance:
+                min_distance = distance
+                closest_fid = g_point['GFID']
+                closest_geo = g_point['geometry']
+
+        fields.at[i, 'GFID'] = closest_fid
+        if closest_fid not in gridmet_targets.keys():
+            gdf = gpd.GeoDataFrame({'geometry': [closest_geo]})
+            first = True
+            for i, r in enumerate(rasters, start=1):
+                stats = zonal_stats(gdf, r, stats=['mean'])[0]['mean']
+                if first:
+                    gridmet_targets[closest_fid] = {i: stats}
+                    first = False
+                else:
+                    gridmet_targets[closest_fid].update({i: stats})
+
+    fields.to_file(fields_join, crs='EPSG:5071')
+
+    len_ = len(gridmet_targets.keys())
+    print('Get gridmet for {} target points'.format(len_))
+    gridmet_pts.index = gridmet_pts['GFID']
+    for k, v in tqdm(gridmet_targets.items(), total=len_):
+        r = gridmet_pts.loc[k]
+        lat, lon = r['lat'], r['lon']
+        g = GridMet(thredds_var, start='1987-01-01', end='2021-12-31', lat=lat, lon=lon)
         df = g.get_point_timeseries()
-        df.to_csv(os.path.join(out_dir_, '{}_{}.csv'.format(r['GFID'], variable)))
+        df.columns = [variable]
+
+        for month in range(1, 13):
+            corr_factor = v[month]
+            idx = [i for i in df.index if i.month == month]
+            df.loc[idx, '{}_corr'.format(variable)] = df.loc[idx, variable] * corr_factor
+
+        _file = os.path.join(gridmet_csv_dir, '{}_{}.csv'.format(r['GFID'], variable))
+        df.to_csv(_file)
 
 
 if __name__ == '__main__':
     d = '/home/dgketchum/PycharmProjects/et-demands/'
 
-    export_openet_correction_surfaces()
+    # export_openet_correction_surfaces()
 
-    raster = os.path.join(d, 'gridmet_corrected', 'correction_surfaces', '')
+    rasters_ = os.path.join(d, 'gridmet_corrected', 'correction_surfaces_aea')
     fields_shp = os.path.join(d, 'examples', 'tongue', 'gis', 'tongue_fields_sample.shp')
     grimet_cent = os.path.join(d, 'examples', 'tongue', 'gis', 'tongue_gridmet_centroids.shp')
-    # gridmet_centroids(raster, fields_shp, grimet_cent)
+    fields_out = os.path.join(d, 'examples', 'tongue', 'gis', 'tongue_fields_sample_gfid.shp')
+    gridmet_dst = os.path.join(d, 'examples', 'tongue', 'climate')
+    corrected_gridmet(fields_shp, grimet_cent, fields_out, gridmet_dst, rasters_, variable='eto')
 
 # ========================= EOF ====================================================================
